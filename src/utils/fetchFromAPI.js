17424dc63a86b5e8e147ff2de51596757d13d390
erick <njoguerick3@gmail.com>
2024-09-12 15:05:10 +0300

    initial commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "x-rapidapi-key": "3130ab5aadmsh8352205261919eep1acb1fjsne7763a699662",
+    "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
